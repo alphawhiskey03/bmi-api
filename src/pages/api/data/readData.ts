171769201d779998b30cb43598a7c719502a4b42
vignesh <vignesh@vigneshs-Air.domain.name>
2023-07-27 00:09:31 +0530

    feat(api): zscore data read route

--- src/pages/api/data/readData.ts
+++ src/pages/api/data/readData.ts
@@ -6,10 +6,17 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    // const result = await insertMaleZScores();
-    // return res.json(result);
+    const result = await insertMaleZScores();
+    return res.status(200).json({
+      success: true,
+      error: null,
+      data: result,
+    });
   } catch (err) {
     console.log(err);
+    return res
+      .status(500)
+      .json({ error: "Something wen't wrong in the server!" });
   }
 };
 
