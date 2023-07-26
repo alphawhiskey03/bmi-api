cf9603fc85aeb2f3a6efd6fa31e34a3942fb791f
vignesh <vignesh@vigneshs-Air.domain.name>
2023-07-26 23:50:08 +0530

    feat(server): middlware for rate limiting and authorization

---
+++ src/middleware.ts
@@ -0,0 +1,61 @@
+import { withAuth } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit";
+import { NextResponse } from "next/server";
+
+const redis = new Redis({
+  url: process.env.REDIS_URL || "",
+  token: process.env.REDIS_TOKEN || "",
+});
+
+const rateLimit = new Ratelimit({
+  redis,
+  limiter: Ratelimit.slidingWindow(5, "1 h"),
+});
+
+export default withAuth(
+  async function middleware(req) {
+    const pathname = req.nextUrl.pathname;
+
+    if (pathname.startsWith("/api")) {
+      const ip = req.ip ?? "127.0.0.1";
+
+      try {
+        const { success } = await rateLimit.limit(ip);
+
+        if (!success) return NextResponse.json({ error: "Too many requests" });
+        return NextResponse.next();
+      } catch (err) {
+        return NextResponse.json({ error: "Internal server error" });
+      }
+    }
+    const token = await getToken({ req });
+    const isAuth = !!token;
+    const authPage = req.nextUrl.pathname.startsWith("/login");
+
+    const sensitiveRoute = ["/dashboard"];
+
+    if (authPage) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL("/dashboard", req.url));
+      }
+    }
+
+    if (!isAuth && sensitiveRoute.some((route) => pathname.startsWith(route))) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  },
+
+  {
+    callbacks: {
+      async authorized() {
+        return true;
+      },
+    },
+  }
+);
+
+// export const config = {
+//   matcher: ["/", "/login", "/dashboard/:path*", "/api/:path*"],
+// };
