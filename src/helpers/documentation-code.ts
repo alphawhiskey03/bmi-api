f0d79a802dabfe1b388c6479ef813bea4ba86735
vignesh <vignesh@vigneshs-Air.domain.name>
2023-07-27 00:05:31 +0530

    chore(*): api documentation data

--- src/helpers/documentation-code.ts
+++ src/helpers/documentation-code.ts
@@ -1,43 +1,142 @@
-export const nodejs = `const axios = require("axios");
+interface CodeObject {
+  nodejs: string;
+  python: string;
+}
 
-const options = {
-    method: 'POST',
-    url: 'https://similarityapi.com/api/v1/similarity',
-    params: {
-      text1: 'First text',
-      text2: 'Second text'
-    },
-    headers: {
-      'Authorization': 'YOUR_API_KEY',
+interface ApiDocumentationSection {
+  heading: string;
+  description: string;
+  code: CodeObject;
+}
+
+interface APIDocumentation {
+  [key: string]: ApiDocumentationSection;
+}
+
+const API_DOCUMENTATION: APIDocumentation = {
+  bmi: {
+    heading: "BMI",
+    description: `<h1 class="text-white">
+    Body mass index (BMI) is a measure of body fat based on height and
+    weight that applies to adult men and women.
+  </h1>
+  `,
+    code: {
+      nodejs: `const axios = require("axios");
+
+    const options = {
+        method: 'POST',
+        url: 'https://bmiapi.com/api/v1/bmi',
+        params: {
+    "height":'<in CM if "metric" and inches if "us" unit preference>',
+    "age":'<1 to 100>',
+    "sex":'<male/female>',
+    "weight":'<in Kg if metric and pounds if US unit preference>',
+    "unitPreference":'<us / metric>'
+},
+        headers: {
+          'Authorization': 'YOUR_API_KEY',
+        }
+      };
+      
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });`,
+      python: `import requests
+
+    url = 'https://bmiapi.com/api/v1/bmi'
+    api_key = 'YOUR_API_KEY'
+    height='<in CM if "metric" and inches if "us" unit preference>'
+    age=30
+    sex="male"
+    weight='<in Kg if metric and pounds if US unit preference>'
+    unitPreference='<us / metric>'
+    
+    headers = {
+        'Authorization': api_key
     }
-  };
-  
-axios.request(options).then(function (response) {
-    console.log(response.data);
-}).catch(function (error) {
-    console.error(error);
-});`;
+    
+    payload = {
+        'height': height,
+        'age': age,
+        'sex': sex,
+        'weight': weight,
+        'unitPreference':unitPreference
+    }
+    
+    response = requests.post(url, headers=headers, json=payload)
+    
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+    else:
+        print(f'Request failed with status code {response.status_code}')`,
+    },
+  },
+  bmr: {
+    heading: "",
+    description: "",
+    code: {
+      nodejs: `const axios = require("axios");
 
-export const python = `import requests
+      const options = {
+          method: 'POST',
+          url: 'https://bmiapi.com/api/v1/bmr',
+          params: {
+      "height":'<in CM if "metric" and inches if "us" unit preference>',
+      "age":'<1 to 100>',
+      "sex":'<male/female>',
+      "weight":'<in Kg if metric and pounds if US unit preference>',
+      "unitPreference":'<us / metric>'
+      "activityFactor":'<sedantary / lightlyActive / moderatelyActive / veryActive / extraActive >'
 
-url = 'https://similarityapi.com/api/v1/similarity'
-api_key = 'YOUR_API_KEY'
-text1 = 'First text'
-text2 = 'Second text'
+  },
+          headers: {
+            'Authorization': 'YOUR_API_KEY',
+          }
+        };
+        
+      axios.request(options).then(function (response) {
+          console.log(response.data);
+      }).catch(function (error) {
+          console.error(error);
+      });`,
+      python: `import requests
 
-headers = {
-    'Authorization': api_key
-}
+      url = 'https://bmiapi.com/api/v1/bmr'
+      api_key = '<YOUR_API_KEY>'
+      height='<in CM if "metric" and inches if "us" unit preference>'
+      age=30
+      sex="male"
+      weight='<in Kg if metric and pounds if US unit preference>'
+      unitPreference='<us / metric>'
+      activityFactor='<sedantary / lightlyActive / moderatelyActive / veryActive / extraActive >'
 
-payload = {
-    'text1': text1,
-    'text2': text2
-}
+      
+      headers = {
+          'Authorization': api_key
+      }
+      
+      payload = {
+          'height': height,
+          'age': age,
+          'sex':sex,
+          'weight':weight,
+          'unitPreference':unitPreference,
+          'activityFactor:activityFactor
+      }
+      
+      response = requests.post(url, headers=headers, json=payload)
+      
+      if response.status_code == 200:
+          data = response.json()
+          print(data)
+      else:
+          print(f'Request failed with status code {response.status_code}')`,
+    },
+  },
+};
 
-response = requests.post(url, headers=headers, json=payload)
-
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-else:
-    print(f'Request failed with status code {response.status_code}')`;
+export default API_DOCUMENTATION;
