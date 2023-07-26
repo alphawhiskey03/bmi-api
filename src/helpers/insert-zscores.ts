ef61fe0ab9f04371705ac72798d063ab50a7d6f6
vignesh <vignesh@vigneshs-Air.domain.name>
2023-07-27 00:47:32 +0530

    fix(api): pass dummy objects to helper function

--- src/helpers/insert-zscores.ts
+++ src/helpers/insert-zscores.ts
@@ -1,10 +1,11 @@
 import { db } from "@/lib/db";
 import { findUniqueRows } from "./utils";
 import { FemaleZscoreData, MaleZscoreData } from "@/types/api";
-const male = require("../../data/male-zscores.json");
-const female = require("../../data/female-zscores.json");
+// const male = require("../../data/male-zscores.json");
+// const female = require("../../data/female-zscores.json");
 
 export const insertMaleZScores = async (): Promise<MaleZscoreData[]> => {
+  const male = {};
   const uniqueData = findUniqueRows(male);
   return Promise.all(
     uniqueData.map((m: any) => {
@@ -19,6 +20,7 @@ export const insertMaleZScores = async (): Promise<MaleZscoreData[]> => {
 };
 
 export const insertFemaleZScores = async (): Promise<FemaleZscoreData[]> => {
+  const female = {};
   const uniqueData = findUniqueRows(female);
   return Promise.all(
     uniqueData.map((m: any) => {
