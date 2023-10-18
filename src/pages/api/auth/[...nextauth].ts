d75456687cc0c2d9a2bfbb1c1e58df0da046ddba
vignesh <vignesh@vigneshs-Air.domain.name>
2023-10-18 13:51:46 +0530

    fix(server): overriding header in next API handler

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,8 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default NextAuth(authOptions);
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader("Cache-Control", "no-store, max-age=0");
+  return NextAuth(req, res, authOptions);
+}
